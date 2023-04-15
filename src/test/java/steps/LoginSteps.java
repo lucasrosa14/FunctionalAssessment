739dbd917838a20f92bb4ba4ec7f698155e40515
lucasrosa14 <contact.lucassr@gmail.com>
2023-04-14 21:46:24 -0300

    Add ScreenshotUtils

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,9 +1,12 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.LoginPage;
+import support.ScreenshotUtils;
 
 public class LoginSteps {
 
@@ -27,5 +30,10 @@ public class LoginSteps {
     @Then("^I should be redirected to the homepage$")
     public void i_should_be_redirected_to_the_homepage() {
         loginPage.checkLoginSuccess();
+    }
+
+    @After
+    public static void afterScenario(Scenario scenario){
+        ScreenshotUtils.addScreenshotOnScenario(scenario);
     }
 }
